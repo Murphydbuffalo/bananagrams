ac34197958dc7e7a4928a3a7ba7dbfdd3074580b
Dan Murphy <murphydbuffalo@gmail.com>
2016-10-07 14:49:18 -0400

    Better formatting for `letterRatios`

--- web/elm/App.elm
+++ web/elm/App.elm
@@ -2,7 +2,6 @@ import Html exposing (Html, h1, div, text, button, br)
 import Html.Attributes exposing (id, class, style)
 import Html.App exposing (programWithFlags)
 import String exposing (join, repeat, split)
--- import WebSocket exposing (listen, send)
 import Dict exposing (Dict, fromList, get, keys)
 import Random exposing (step, list, int)
 import Array exposing (initialize, toList)
@@ -25,32 +24,11 @@ tilesPerPlayer = 21
 letterRatios : Dict String Int
 letterRatios =
   Dict.fromList [
-    ("A", 13),
-    ("B", 3),
-    ("C", 3),
-    ("D", 6),
-    ("E", 18),
-    ("F", 3),
-    ("G", 4),
-    ("H", 3),
-    ("I", 12),
-    ("J", 2),
-    ("K", 2),
-    ("L", 5),
-    ("M", 3),
-    ("N", 8),
-    ("O", 11),
-    ("P", 3),
-    ("Q", 2),
-    ("R", 9),
-    ("S", 6),
-    ("T", 9),
-    ("U", 6),
-    ("V", 3),
-    ("W", 3),
-    ("X", 2),
-    ("Y", 3),
-    ("Z", 2)
+    ("A" => 13), ("B" => 3), ("C" => 3), ("D" => 6), ("E" => 18), ("F" => 3),
+    ("G" => 4), ("H" => 3), ("I" => 12), ("J" => 2), ("K" => 2), ("L" => 5),
+    ("M" => 3), ("N" => 8), ("O" => 11), ("P" => 3), ("Q" => 2), ("R" => 9),
+    ("S" => 6), ("T" => 9), ("U" => 6), ("V" => 3), ("W" => 3), ("X" => 2),
+    ("Y" => 3), ("Z" => 2)
   ]
 
 -- TYPE DEFINITIONS
